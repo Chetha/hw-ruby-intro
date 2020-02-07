4c917e775a75d0c68fb0aa06b1fae47a72799418
Ubuntu <ubuntu@ip-172-31-80-214.ec2.internal>
2020-02-07 16:05:41 +0000

    Part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,9 +2,14 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
-end
+#def sum arr
+ # sum = 0
+  #puts arr
+  #arr.each do |i|
+   # sum += i
+  #return sum
+#end
+
 
 def max_2_sum arr
   # YOUR CODE HERE
@@ -28,8 +33,31 @@ def binary_multiple_of_4? s
   # YOUR CODE HERE
 end
 
-# Part 3
+#Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError if  isbn == nil || isbn.length == 0
+    raise ArgumentError if price <= 0
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+    attr_accessor :isbn # sugar for generating getters/setters
+
+  def price
+    @price
+  end
+  
+  def price=(price)
+    @price = price
+  end
+  
+  def price_as_string
+    "$%0.2f" % @price
+  end
+
 end
+
